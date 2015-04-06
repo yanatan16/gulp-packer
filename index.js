be068de7504b9cf4e9d2301f6ff7465e4acf217f
ViZhe <vizhe.cat@ya.ru>
2015-04-07 01:02:15 +0300

    postfix add

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ module.exports = function (opt) {
   opt = opt || {}
   opt.base62 = opt.base62 || false
   opt.shrink = opt.shrink || false
+  opt.postfix = opt.postfix || '.min'
 
   function minifyFile(file) {
     if (file.isNull()) return this.emit('data', file) // pass along
     if (file.isStream()) return this.emit('error', new Error('gulp-packer: Streaming not supported'))
 
     var str = file.contents.toString('utf8')
-      , dest = gutil.replaceExtension(file.path, '.min.js')
+      , dest = gutil.replaceExtension(file.path, opt.postfix + '.js')
       , data = packer.pack(str, opt.base62, opt.shrink)
 
     file.path = dest
